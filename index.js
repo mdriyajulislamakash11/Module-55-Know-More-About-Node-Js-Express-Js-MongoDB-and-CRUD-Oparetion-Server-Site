da3a47b48875b2f5ee7b1492bf31b8dc4c491b48
Akash <hafejmohammadakash@gmail.com>
2025-05-17 11:06:24 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MiddleWare
 app.use(cors())
 app.use(express.json())
+
+// akashahmed
+// hMyb5CSbnGwIVczL
+
+
+const uri = "mongodb+srv://akashahmed:hMyb5CSbnGwIVczL@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
@@ -17,5 +47,5 @@ app.get("/", (rec, res) => {
 
 app.listen(port, () => {
     console.log(`CURD id running Port: ${port}`)
-})
+});
 
