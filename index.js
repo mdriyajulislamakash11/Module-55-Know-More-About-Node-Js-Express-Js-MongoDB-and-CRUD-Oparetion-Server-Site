c250a8697ba5bfcf27124c90e650b8446dc22be5
Akash <hafejmohammadakash@gmail.com>
2025-05-17 21:06:38 +0600

    mongoDB post methods

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MiddleWare
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // akashahmed
 // hMyb5CSbnGwIVczL
 
-
-const uri = "mongodb+srv://akashahmed:hMyb5CSbnGwIVczL@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://akashahmed:hMyb5CSbnGwIVczL@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,16 +20,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("Somple-users-DB");
+    const userCollection = database.collection("users");
+
+    app.post("/users", async (rec, res) => {
+      const user = rec.body;
+      console.log("new user: ", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,15 +50,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (rec, res) => {
-    res.send("Server is running")
+  res.send("Server is running");
 });
-
-
 
 app.listen(port, () => {
-    console.log(`CURD id running Port: ${port}`)
+  console.log(`CURD id running Port: ${port}`);
 });
-
